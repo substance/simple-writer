423da4cd084af0c141990baa9c55d36f89f0d41d
Michael Aufreiter <michael@substance.io>
2016-10-01 19:16:20 +0200

    Cleanup.

--- src/simple-writer/HTMLArticleImporter.js
+++ src/simple-writer/HTMLArticleImporter.js
@@ -2,11 +2,7 @@ import { HTMLImporter } from 'substance'
 import isArray from 'lodash/isArray'
 import find from 'lodash/find'
 
-/**
- * NewsMLImporter Importer
- */
 class HTMLArticleImporter extends HTMLImporter {
-
   convertDocument(htmlEl) {
     var bodyEl = htmlEl.find('body')
     this.convertElement(bodyEl);
