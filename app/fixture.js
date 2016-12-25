ad1252af867a851c58b642585ba25449cb49ee56
Michael Aufreiter <michael@substance.io>
2016-12-25 22:38:16 +0100

    Remove whitespace from fixture.

--- app/fixture.js
+++ app/fixture.js
@@ -3,10 +3,6 @@
 // break the converter
 export default `<html><body>
   <h1>SimpleWriter</h1>
-  <p>
-    This is the official
-    <span data-type="comment" data-comment="A JavaScript library for web-based content editing">Substance</span>
-    editor boilerplate example. Fork it, and create your own editor.
-  </p>
+  <p>This is the official <span data-type="comment" data-comment="A JavaScript library for web-based content editing">Substance</span> editor boilerplate example. Fork it, and create your own editor.</p>
   <p>You can find the source code on <a href="http://github.com/substance/starter">Github</a>.</p>
 </body></html>`
