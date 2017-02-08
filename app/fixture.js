8a1d1227762a14b153b9b9de5d47a64cb8deadc5
Michael Aufreiter <michael@substance.io>
2017-02-08 17:21:43 +0100

    Use el.getChildren() to filter only elements.

--- app/fixture.js
+++ app/fixture.js
@@ -1,6 +1,3 @@
-// NOTE: We must ensure there is no whitespace before or after the <html> element
-// otherwise CheerioDOMElement will not recognize it as a single element, which will
-// break the converter
 export default `<html><body>
   <h1>SimpleWriter</h1>
   <p>This is the official <span data-type="comment" data-comment="A JavaScript library for web-based content editing">Substance</span> editor boilerplate example. Fork it, and create your own editor.</p>
